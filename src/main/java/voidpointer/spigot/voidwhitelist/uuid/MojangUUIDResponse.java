77e5c2f76b63510fb261b8f8888e25956ed24a75
Vasiliy <void.pointer@ya.ru>
2022-03-29 01:53:02 +0500

    Make code much more readable using Guava

---
+++ src/main/java/voidpointer/spigot/voidwhitelist/uuid/MojangUUIDResponse.java
@@ -0,0 +1,11 @@
+package voidpointer.spigot.voidwhitelist.uuid;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+final class MojangUUIDResponse {
+    String name;
+    String id;
+}
--- src/main/java/voidpointer/spigot/voidwhitelist/uuid/UniversalUUIDFetcher.java
+++ src/main/java/voidpointer/spigot/voidwhitelist/uuid/UniversalUUIDFetcher.java
@@ -2,14 +2,13 @@ package voidpointer.spigot.voidwhitelist.uuid;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import voidpointer.spigot.framework.localemodule.LocaleLog;
 import voidpointer.spigot.framework.localemodule.annotation.AutowiredLocale;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Optional;
 import java.util.UUID;
@@ -18,7 +17,8 @@ import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
 public final class UniversalUUIDFetcher implements UUIDFetcher {
-    private static final String UUID_URL = "https://api.mojang.com/users/profiles/minecraft/";
+    private static final String UUID_API_URL = "https://api.mojang.com/users/profiles/minecraft/";
+    private static final Gson gson = new GsonBuilder().create();
     @AutowiredLocale private static LocaleLog log;
     private final Cache<String, UUID> onlineUuidCache = CacheBuilder.newBuilder()
             .expireAfterAccess(6L, TimeUnit.HOURS)
@@ -54,73 +54,45 @@ public final class UniversalUUIDFetcher implements UUIDFetcher {
             return CompletableFuture.completedFuture(Optional.of(uniqueId));
 
         return CompletableFuture.supplyAsync(() -> {
-            String response = callURL(UUID_URL + name);
-            if (response == null)
+            UUID uniqueIdResponse = callApi(name);
+            if (uniqueIdResponse == null)
                 return Optional.empty();
 
-            String uniqueIdStr = readUniqueIdFromResponse(response);
-            Optional<UUID> optionalUUID = Optional.of(UUID.fromString(uniqueIdStr));
+            Optional<UUID> optionalUUID = Optional.of(uniqueIdResponse);
             onlineUuidCache.put(name, optionalUUID.get());
             return optionalUUID;
         });
     }
 
-    private String callURL(final String urlStr) {
-        URL url;
+    private UUID callApi(final String name) {
         try {
-            url = new URL(urlStr);
-        } catch (MalformedURLException malformedURLException) {
-            log.severe("Invalid URL: {0}", urlStr);
-            return null;
-        }
-        HttpURLConnection connection;
-        try {
-            connection = (HttpURLConnection) url.openConnection();
+            String id = requestApiUrl(name);
+            String uuid = idToUuid(id);
+            return UUID.fromString(uuid);
         } catch (IOException ioException) {
-            log.severe("Couldn't open HTTP connection: ",  ioException);
+            log.warn("Unable to request Mojang API", ioException);
+            return null;
+        } catch (IllegalArgumentException illegalArgumentException) {
+            log.warn("Invalid UUID format", illegalArgumentException);
             return null;
         }
-        try {
-            if (connection.getResponseCode() == 204) {
-                log.warn("Requested player not found (url: {0})", urlStr);
-                return null;
-            } else if (connection.getInputStream() == null) {
-                log.severe("No InputStream in connection");
-                return null;
-            }
-        } catch (IOException ioException) {
-            log.severe(ioException.getMessage());
-            return null;
-        }
-        connection.setReadTimeout(60 * 1000 /* 60 sec */);
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        try (InputStream in = connection.getInputStream()) {
-            byte[] buffer = new byte[1024];
-            for (int len; (len = in.read(buffer)) != -1; )
-                out.write(buffer, 0, len);
-        } catch (IOException ioException) {
-            log.severe("Unable to fetch UUID: I/O failure");
-            ioException.printStackTrace();
-            return null;
-        }
-        return out.toString();
     }
 
-    private String readUniqueIdFromResponse(final String toRead) {
-        final StringBuilder result = new StringBuilder();
-        /* toRead is a string of format "{"name":"_voidpointer","id":"c55a15b5896f4c099c0775ad36572aad"}"
-         * so this method reads the "id" property from the end of that string
-         * and additionally adds '-' char where it's needed to make the id look like
-         * c55a15b5-896f-4c09-9c07-75ad36572aad */
-        for (int i = toRead.length() - 3, j = 31; i >= 0; i--, j--) {
-            if (toRead.charAt(i) != '"') {
-                if ((j == 19) || (j == 15) || (j == 11) || (j == 7))
-                    result.insert(0, '-');
-                result.insert(0, toRead.charAt(i));
-            } else {
-                break;
-            }
+    private String idToUuid(final String id) {
+        StringBuilder uuidBuilder = new StringBuilder(id.length() + 4);
+        for (int index = 0; index < id.length(); index++) {
+            uuidBuilder.append(id.charAt(index));
+            if ((index == 7) || (index == 11) || (index == 15) || (index == 19))
+                uuidBuilder.append('-');
         }
-        return result.toString();
+        return uuidBuilder.toString();
+    }
+
+    private String requestApiUrl(final String name) throws IOException {
+        return gson.fromJson(newApiRequestConnection(name), MojangUUIDResponse.class).id;
+    }
+
+    private InputStreamReader newApiRequestConnection(final String name) throws IOException {
+        return new InputStreamReader(new URL(UUID_API_URL + name).openStream());
     }
 }
