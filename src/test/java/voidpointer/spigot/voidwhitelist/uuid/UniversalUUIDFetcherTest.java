2abe6dfccd74a0a779c72d623864b67ad63ee752
Vasiliy <void.pointer@ya.ru>
2022-03-29 02:03:33 +0500

    Add more test cases

--- src/test/java/voidpointer/spigot/voidwhitelist/uuid/UniversalUUIDFetcherTest.java
+++ src/test/java/voidpointer/spigot/voidwhitelist/uuid/UniversalUUIDFetcherTest.java
@@ -94,7 +94,16 @@ class UniversalUUIDFetcherTest {
 
     static Stream<Arguments> getIdUuid() {
         return Stream.of(
-                arguments("c55a15b5896f4c099c0775ad36572aad", "c55a15b5-896f-4c09-9c07-75ad36572aad")
+                arguments("c55a15b5896f4c099c0775ad36572aad", "c55a15b5-896f-4c09-9c07-75ad36572aad"),
+                arguments("069a79f444e94726a5befca90e38aaf5", "069a79f4-44e9-4726-a5be-fca90e38aaf5"),
+                arguments("61699b2ed3274a019f1e0ea8c3f06bc6", "61699b2e-d327-4a01-9f1e-0ea8c3f06bc6"),
+                arguments("9c2ac9585de945a88ca14122eb4c0b9e", "9c2ac958-5de9-45a8-8ca1-4122eb4c0b9e"),
+                arguments("52ea935499ed4b06bec2331e7c0f6f57", "52ea9354-99ed-4b06-bec2-331e7c0f6f57"),
+                arguments("3d6e661680293e868d408b5bbbd0cc2a", "3d6e6616-8029-3e86-8d40-8b5bbbd0cc2a"),
+                arguments("b50ad385829d3141a2167e7d7539ba7f", "b50ad385-829d-3141-a216-7e7d7539ba7f"),
+                arguments("4d258a8123583084816605b9faccad80", "4d258a81-2358-3084-8166-05b9faccad80"),
+                arguments("0310df31090637c1b92c231dd867f622", "0310df31-0906-37c1-b92c-231dd867f622"),
+                arguments("67d8f00eec193a06974711f67788288a", "67d8f00e-ec19-3a06-9747-11f67788288a")
         );
     }
 }
