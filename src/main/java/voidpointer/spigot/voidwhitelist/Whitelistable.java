c76d905d2d150df9cec5156098c9f4343400672d
Vasiliy <void.pointer@ya.ru>
2022-04-03 23:41:57 +0500

    Consider adding name property in the future

--- src/main/java/voidpointer/spigot/voidwhitelist/Whitelistable.java
+++ src/main/java/voidpointer/spigot/voidwhitelist/Whitelistable.java
@@ -36,6 +36,17 @@ public interface Whitelistable {
 
     UUID getUniqueId();
 
+    /* TODO:
+     *  Consider adding a String name property.
+     *  This would require changing WhitelistService#add() method to accept
+     *      name as well as required UUID and non-required date. It seems like
+     *      the name is available for all method executors.
+     *  This way the API will allow showing Whitelistable's in-game name avoiding
+     *      a search through Bukkit API or Mojang web requests.
+     *  Though, it will require automatic name updates on Join events. Perhaps,
+     *      some notifications & configuration needed.
+     */
+
     /**
      * Checks whether this entity is associated with provided player or not.
      *
