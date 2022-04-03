004046f25f24bdd309e3bd15d5589d620c0c0b09
Vasiliy <void.pointer@ya.ru>
2022-04-04 00:23:12 +0500

    Implement new WhitelistService API

--- src/main/java/voidpointer/spigot/voidwhitelist/storage/CachedWhitelistService.java
+++ src/main/java/voidpointer/spigot/voidwhitelist/storage/CachedWhitelistService.java
@@ -19,20 +19,67 @@ import lombok.Getter;
 import lombok.Setter;
 import voidpointer.spigot.voidwhitelist.Whitelistable;
 
+import java.util.Collections;
 import java.util.Date;
-import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Optional;
-import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentSkipListSet;
 
+import static java.util.concurrent.CompletableFuture.completedFuture;
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+
+/**
+ * An abstract and thread-safe implementation for all {@link WhitelistService} operations.
+ *
+ * This implementation stores all {@link Whitelistable} entries in memory
+ *  using {@link java.util.Set} and does not implement saving the whitelist.
+ * 
+ * In order to save the cached whitelist entries you need to implement
+ *  {@link #saveWhitelist()} methods. It will be invoked after any modification
+ *  <em>({@link #add(UUID)}, {@link #remove(Whitelistable)} etc.)</em>to the
+ *  cached whitelist.
+ */
 public abstract class CachedWhitelistService implements WhitelistService {
     @Getter(AccessLevel.PROTECTED)
     @Setter(AccessLevel.PROTECTED)
-    private Set<Whitelistable> cachedWhitelist = new HashSet<>();
+    private ConcurrentSkipListSet<Whitelistable> cachedWhitelist = new ConcurrentSkipListSet<>();
+
+    @Override public CompletableFuture<SortedSet<Whitelistable>> findAll(final Whitelistable offset, final int limit) {
+        if (cachedWhitelist.isEmpty())
+            return completedFuture(Collections.emptySortedSet());
+        return supplyAsync(() -> {
+            SortedSet<Whitelistable> subset = new TreeSet<>();
+            Iterator<Whitelistable> iterator = cachedWhitelist.iterator();
+            while (iterator.hasNext() && !iterator.next().equals(offset))
+                ;
+            for (int index = 0; (index < limit) && iterator.hasNext(); index++, subset.add(iterator.next()))
+                ;
+            return subset;
+        });
+    }
+
+    @Override public CompletableFuture<Optional<Whitelistable>> findFirst() {
+        if (!cachedWhitelist.isEmpty())
+            return completedFuture(Optional.of(cachedWhitelist.first()));
+        return completedFuture(Optional.empty());
+    }
+
+    @Override public CompletableFuture<Optional<Whitelistable>> findLast() {
+        if (!cachedWhitelist.isEmpty())
+            return completedFuture(Optional.of(cachedWhitelist.last()));
+        return completedFuture(Optional.empty());
+    }
+
+    @Override public CompletableFuture<Integer> size() {
+        return completedFuture(cachedWhitelist.size());
+    }
 
     @Override public CompletableFuture<Optional<Whitelistable>> find(final UUID uuid) {
-        return CompletableFuture.supplyAsync(() -> {
+        return supplyAsync(() -> {
             // Could've used Map for fast search operations, but who tf cares
             for (Whitelistable whitelistable : cachedWhitelist) {
                 if (whitelistable.getUniqueId().equals(uuid))
@@ -47,7 +94,7 @@ public abstract class CachedWhitelistService implements WhitelistService {
     }
 
     @Override public CompletableFuture<Whitelistable> add(final UUID uuid, final Date expiresAt) {
-        return CompletableFuture.supplyAsync(() -> {
+        return supplyAsync(() -> {
             Whitelistable whitelistable = new SimpleWhitelistable(uuid, expiresAt);
             if (!cachedWhitelist.add(whitelistable)) {
                 cachedWhitelist.remove(whitelistable);
@@ -59,7 +106,7 @@ public abstract class CachedWhitelistService implements WhitelistService {
     }
 
     @Override public CompletableFuture<Boolean> remove(final Whitelistable whitelistable) {
-        return CompletableFuture.supplyAsync(() -> {
+        return supplyAsync(() -> {
             cachedWhitelist.remove(whitelistable);
             saveWhitelist();
             return true;
