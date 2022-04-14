df30bd81cf03f1a5154d39040a229f6153fc3db5
Vasiliy <void.pointer@ya.ru>
2022-04-15 01:33:28 +0500

    Implement importing

--- src/main/java/voidpointer/spigot/voidwhitelist/command/ImportJsonCommand.java
+++ src/main/java/voidpointer/spigot/voidwhitelist/command/ImportJsonCommand.java
@@ -14,13 +14,36 @@
  */
 package voidpointer.spigot.voidwhitelist.command;
 
+import org.bukkit.plugin.Plugin;
+import voidpointer.spigot.framework.di.Autowired;
+import voidpointer.spigot.framework.localemodule.Locale;
+import voidpointer.spigot.framework.localemodule.annotation.AutowiredLocale;
+import voidpointer.spigot.voidwhitelist.Whitelistable;
+import voidpointer.spigot.voidwhitelist.storage.StorageFactory;
+import voidpointer.spigot.voidwhitelist.storage.WhitelistService;
+import voidpointer.spigot.voidwhitelist.storage.db.OrmliteWhitelistService;
+import voidpointer.spigot.voidwhitelist.storage.json.JsonWhitelistService;
+
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+
+import static java.lang.System.currentTimeMillis;
+import static voidpointer.spigot.voidwhitelist.command.ImportOptions.REPLACE;
+import static voidpointer.spigot.voidwhitelist.message.WhitelistMessage.*;
+import static voidpointer.spigot.voidwhitelist.storage.StorageMethod.JSON;
+import static voidpointer.spigot.voidwhitelist.storage.json.JsonWhitelistService.WHITELIST_FILE_NAME;
 
 public class ImportJsonCommand extends Command {
     public static final String NAME = "import-json";
     public static final String PERMISSION = "permission";
     public static final List<String> ALIASES = Collections.singletonList("importjson");
+
+    @AutowiredLocale private static Locale locale;
+    @Autowired private static WhitelistService whitelistService;
+    @Autowired private static StorageFactory storageFactory;
+    @Autowired(mapId="plugin")
+    private static Plugin plugin;
 
     public ImportJsonCommand() {
         super(NAME);
@@ -29,7 +52,32 @@ public class ImportJsonCommand extends Command {
     }
 
     @Override public void execute(final Args args) {
+        if (!(whitelistService instanceof OrmliteWhitelistService)) {
+            locale.localize(IMPORT_ONLY_TO_DATABASE).send(args.getSender());
+            return;
+        }
+        plugin.getServer().getScheduler().runTaskAsynchronously(plugin, () -> execute0(args));
+    }
 
+    private void execute0(final Args args) {
+        final long start = currentTimeMillis();
+        JsonWhitelistService json = (JsonWhitelistService) storageFactory.loadStorage(JSON);
+        locale.localize(IMPORT_LOADED)
+                .set("loaded", json.getWhitelist().size())
+                .set("storage", WHITELIST_FILE_NAME)
+                .send(args.getSender());
+        OrmliteWhitelistService database = (OrmliteWhitelistService) ImportJsonCommand.whitelistService;
+        Set<Whitelistable> imported;
+        if (args.hasOption(REPLACE))
+            imported = database.addAllReplacing(json.getWhitelist()).join();
+        else
+            imported = database.addAllIfNotExists(json.getWhitelist()).join();
+        final long end = currentTimeMillis();
+        locale.localize(IMPORT_RESULT)
+                .set("imported", imported.size())
+                .set("loaded", json.getWhitelist().size())
+                .set("ms-spent", end - start)
+                .send(args.getSender());
     }
 
     @Override public List<String> getAliases() {
