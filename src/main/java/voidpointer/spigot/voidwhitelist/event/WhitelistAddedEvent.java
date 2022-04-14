00127c69b61230a8a47969e24952a9062ba51677
Vasiliy <void.pointer@ya.ru>
2022-04-15 02:02:00 +0500

    semantic: update annotations

--- src/main/java/voidpointer/spigot/voidwhitelist/event/WhitelistAddedEvent.java
+++ src/main/java/voidpointer/spigot/voidwhitelist/event/WhitelistAddedEvent.java
@@ -15,27 +15,27 @@
 package voidpointer.spigot.voidwhitelist.event;
 
 import lombok.Getter;
-import lombok.NonNull;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.checkerframework.checker.nullness.qual.NonNull;
 import voidpointer.spigot.voidwhitelist.Whitelistable;
 
-@Getter
 public final class WhitelistAddedEvent extends Event {
     private static final HandlerList handlers = new HandlerList();
 
-    @NonNull private final Whitelistable whitelistable;
+    private @Getter @NonNull final Whitelistable whitelistable;
 
     public static HandlerList getHandlerList() {
         return handlers;
     }
 
-    public WhitelistAddedEvent(final Whitelistable whitelistable) {
+    public WhitelistAddedEvent(final @NonNull Whitelistable whitelistable) {
         super(true);
         this.whitelistable = whitelistable;
     }
 
-    @Override public HandlerList getHandlers() {
+    @Override
+    public @NonNull HandlerList getHandlers() {
         return handlers;
     }
 }
