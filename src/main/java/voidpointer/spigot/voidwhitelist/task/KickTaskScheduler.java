cb38f4d71fc332fe0e574996155907770b3f5976
Vasiliy <void.pointer@ya.ru>
2022-04-27 00:06:53 +0500

    Add #schedule(Iterable<Player>) method

--- src/main/java/voidpointer/spigot/voidwhitelist/task/KickTaskScheduler.java
+++ src/main/java/voidpointer/spigot/voidwhitelist/task/KickTaskScheduler.java
@@ -5,9 +5,10 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitTask;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import voidpointer.spigot.framework.di.Autowired;
-import voidpointer.spigot.framework.localemodule.Locale;
+import voidpointer.spigot.framework.localemodule.LocaleLog;
 import voidpointer.spigot.framework.localemodule.annotation.AutowiredLocale;
 import voidpointer.spigot.voidwhitelist.Whitelistable;
+import voidpointer.spigot.voidwhitelist.storage.WhitelistService;
 
 import java.util.Map;
 import java.util.Optional;
@@ -18,9 +19,26 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static voidpointer.spigot.voidwhitelist.message.WhitelistMessage.*;
 
 public final class KickTaskScheduler {
-    @AutowiredLocale private static Locale locale;
+    @AutowiredLocale private static LocaleLog locale;
     @Autowired(mapId="plugin") private static Plugin plugin;
+    @Autowired private static WhitelistService whitelistService;
     private final Map<Player, BukkitTask> tasks = new ConcurrentHashMap<>();
+
+    public void schedule(final @NonNull Iterable<Player> players) {
+        for (final Player player : players) {
+            if (!player.isOnline())
+                continue;
+            whitelistService.find(player.getUniqueId()).thenAcceptAsync(optionalWhitelistable -> {
+                if (optionalWhitelistable.isPresent())
+                    schedule(optionalWhitelistable.get());
+                else
+                    kickSynchronously(player);
+            }).exceptionally(thrown -> {
+                locale.warn("Error on kick schedule: {0}", thrown.getMessage());
+                return null;
+            });
+        }
+    }
 
     public void schedule(final @NonNull Whitelistable whitelistable) {
         final Optional<Player> player = whitelistable.findAssociatedOnlinePlayer();
