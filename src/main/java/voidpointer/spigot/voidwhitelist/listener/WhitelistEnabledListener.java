d89818a4b903c112d94510ccc582a6915e85d5e3
Vasiliy <void.pointer@ya.ru>
2022-04-27 00:55:04 +0500

    Use KickTaskSchedule#schedule(Iterable<? extends Player>)

--- src/main/java/voidpointer/spigot/voidwhitelist/listener/WhitelistEnabledListener.java
+++ src/main/java/voidpointer/spigot/voidwhitelist/listener/WhitelistEnabledListener.java
@@ -15,44 +15,26 @@
 package voidpointer.spigot.voidwhitelist.listener;
 
 import lombok.RequiredArgsConstructor;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import voidpointer.spigot.framework.di.Autowired;
-import voidpointer.spigot.framework.localemodule.LocaleLog;
-import voidpointer.spigot.framework.localemodule.annotation.AutowiredLocale;
 import voidpointer.spigot.voidwhitelist.event.WhitelistEnabledEvent;
-import voidpointer.spigot.voidwhitelist.storage.WhitelistService;
 import voidpointer.spigot.voidwhitelist.task.KickTaskScheduler;
+
+import static org.bukkit.Bukkit.getOnlinePlayers;
 
 @RequiredArgsConstructor
 public final class WhitelistEnabledListener implements Listener {
-    @AutowiredLocale private static LocaleLog locale;
     @Autowired private static KickTaskScheduler kickTaskScheduler;
-    @Autowired private static WhitelistService whitelistService;
-    @Autowired(mapId="plugin")
-    private static Plugin plugin;
 
     /**
-     * Kick all non whitelisted online players and schedule kick tasks
-     *  for those who's time will expire.
+     * <p>Reschedule kick tasks for online players.</p>
      */
     @EventHandler(priority=EventPriority.HIGHEST)
     public void onEnabled(final WhitelistEnabledEvent event) {
-        for (final Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-            whitelistService.find(onlinePlayer.getUniqueId()).thenAcceptAsync(whitelistable -> {
-                if (whitelistable.isPresent()) // TODO Java 16 migrate to #ifPresetOrElse()
-                    kickTaskScheduler.schedule(whitelistable.get());
-                else
-                    kickTaskScheduler.kickSynchronously(onlinePlayer);
-            }).whenComplete((res, th) -> {
-                if (th != null)
-                    locale.warn("Couldn't schedule a kick task on whitelist enable event", th);
-            });
-        }
+        kickTaskScheduler.schedule(getOnlinePlayers());
     }
 
     public void register() {
