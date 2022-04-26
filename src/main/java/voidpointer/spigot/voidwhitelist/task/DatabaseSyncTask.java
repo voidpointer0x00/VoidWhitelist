4688a0111f1d6f46c4a5a70da7ce0288bde206ec
Vasiliy <void.pointer@ya.ru>
2022-04-27 02:00:08 +0500

    Implement DatabaseSyncTask

--- src/main/java/voidpointer/spigot/voidwhitelist/task/DatabaseSyncTask.java
+++ src/main/java/voidpointer/spigot/voidwhitelist/task/DatabaseSyncTask.java
@@ -2,12 +2,26 @@ package voidpointer.spigot.voidwhitelist.task;
 
 import org.bukkit.scheduler.BukkitRunnable;
 import voidpointer.spigot.framework.di.Autowired;
+import voidpointer.spigot.voidwhitelist.config.WhitelistConfig;
 import voidpointer.spigot.voidwhitelist.storage.WhitelistService;
 
 public final class DatabaseSyncTask extends BukkitRunnable {
+    @Autowired private static WhitelistConfig whitelistConfig;
+    @Autowired private static KickTaskScheduler kickTaskScheduler;
     @Autowired private static WhitelistService whitelistService;
 
     @Override public void run() {
-
+        if (!whitelistConfig.isWhitelistEnabled())
+            return;
+        for (final KickTask kickTask : kickTaskScheduler.getTasks()) {
+            whitelistService.find(kickTask.player.getUniqueId()).thenAcceptAsync(optionalWhitelistable -> {
+                if (!optionalWhitelistable.isPresent()) {
+                    kickTaskScheduler.kickSynchronously(kickTask.player);
+                } else if (optionalWhitelistable.get().isExpirable()) {
+                    if (kickTask.expiresAt != optionalWhitelistable.get().getExpiresAt().getTime())
+                        kickTaskScheduler.schedule(optionalWhitelistable.get());
+                }
+            });
+        }
     }
 }
