8952d4ce2197080e611fc99af6c2054b698abef3
Vasiliy <void.pointer@ya.ru>
2022-04-30 01:56:12 +0500

    Fix: use #find() method instead of #matches()

--- src/main/java/voidpointer/spigot/voidwhitelist/version/Version.java
+++ src/main/java/voidpointer/spigot/voidwhitelist/version/Version.java
@@ -1,12 +1,11 @@
 package voidpointer.spigot.voidwhitelist.version;
 
-import voidpointer.spigot.voidwhitelist.gui.WhitelistGui;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static java.lang.Integer.parseInt;
 import static org.bukkit.Bukkit.getBukkitVersion;
+import static voidpointer.spigot.voidwhitelist.gui.WhitelistGui.MAJOR_VERSION_REQUIRED;
 
 public final class Version {
     public static final Pattern VERSION_PATTERN = Pattern.compile("\\d+\\.(?<major>\\d+)\\.(?<minor>\\d+)");
@@ -16,8 +15,7 @@ public final class Version {
     public static boolean supportsGui() {
         if (supportsGui == null) {
             final Matcher versionMatcher = VERSION_PATTERN.matcher(getBukkitVersion());
-            supportsGui = versionMatcher.matches()
-                    && parseInt(versionMatcher.group("major")) < WhitelistGui.MAJOR_VERSION_REQUIRED;
+            supportsGui = versionMatcher.find() && parseInt(versionMatcher.group("major")) < MAJOR_VERSION_REQUIRED;
         }
         return supportsGui;
     }
