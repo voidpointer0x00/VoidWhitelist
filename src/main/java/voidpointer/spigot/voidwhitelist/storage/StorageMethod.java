d1669bcea25af90352542ff3903f7c8da96301cf
Vasiliy <void.pointer@ya.ru>
2023-03-24 08:51:37 +0500

    Cut the Serial storage out

--- src/main/java/voidpointer/spigot/voidwhitelist/storage/StorageMethod.java
+++ src/main/java/voidpointer/spigot/voidwhitelist/storage/StorageMethod.java
@@ -16,15 +16,8 @@ package voidpointer.spigot.voidwhitelist.storage;
 
 import voidpointer.spigot.voidwhitelist.storage.db.OrmliteWhitelistService;
 import voidpointer.spigot.voidwhitelist.storage.json.JsonWhitelistService;
-import voidpointer.spigot.voidwhitelist.storage.serial.SerialWhitelistService;
 
 public enum StorageMethod {
-    /**
-     * Based on standard Java serialization.
-     *
-     * @see SerialWhitelistService
-     */
-    SERIAL,
     /**
      * Based on JSON serialization libraries. Considered to be more
      *  human-friendly than serialization and fast enough.
