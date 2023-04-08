07f16304f0364b6f80f20b2500dbdf856c951141
Vasiliy <void.pointer@ya.ru>
2023-04-08 20:41:18 +0500

    Change & document format

--- src/main/java/voidpointer/spigot/voidwhitelist/storage/json/JsonAutoWhitelist.java
+++ src/main/java/voidpointer/spigot/voidwhitelist/storage/json/JsonAutoWhitelist.java
@@ -12,19 +12,31 @@ import java.util.Optional;
 import java.util.UUID;
 
 final class JsonAutoWhitelist extends JsonStorage {
+    /**
+     * <code>
+     *     {
+     *         "autoWhitelist": [
+     *              {
+     *                  "uniqueId": "c55a15b5-896f-4c09-9c07-75ad36572aad",
+     *                  "timesAutoWhitelisted": 1
+     *              }, ...
+     *         ]
+     *     }
+     * </code>
+     */
     public static Optional<Map<UUID, AutoWhitelistNumber>> parseJsonFile(final File jsonFile) {
         final String jsonContents = fileToString(jsonFile);
         if (jsonContents == null)
             return Optional.empty();
         try {
             JsonObject dataAndMeta = gson.fromJson(jsonContents, JsonObject.class);
-            JsonArray autoWhitelist = dataAndMeta.getAsJsonArray("auto-whitelist");
+            JsonArray autoWhitelist = dataAndMeta.getAsJsonArray("autoWhitelist");
             if (autoWhitelist == null)
                 return Optional.empty();
             Map<UUID, AutoWhitelistNumber> parsed = new HashMap<>();
             autoWhitelist.forEach(jsonElement -> parsed.put(
-                    UUID.fromString(jsonElement.getAsJsonObject().get("uuid").getAsString()),
-                    AutoWhitelistNumber.of(jsonElement.getAsJsonObject().get("times-auto-whitelisted").getAsInt())
+                    UUID.fromString(jsonElement.getAsJsonObject().get("uniqueId").getAsString()),
+                    AutoWhitelistNumber.of(jsonElement.getAsJsonObject().get("timesAutoWhitelisted").getAsInt())
             ));
             return Optional.of(parsed);
         } catch (final JsonSyntaxException | NullPointerException | IllegalArgumentException exception) {
