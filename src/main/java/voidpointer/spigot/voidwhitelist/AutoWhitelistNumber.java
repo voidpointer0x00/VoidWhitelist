77d0a71fa5d767ba5fc3626ee229a11422340fbe
Vasiliy <void.pointer@ya.ru>
2023-04-08 19:40:58 +0500

    Add `#ZERO` implementation & default implementation for `#isExceeded()`

--- src/main/java/voidpointer/spigot/voidwhitelist/AutoWhitelistNumber.java
+++ src/main/java/voidpointer/spigot/voidwhitelist/AutoWhitelistNumber.java
@@ -1,7 +1,11 @@
 package voidpointer.spigot.voidwhitelist;
 
 public interface AutoWhitelistNumber {
+    AutoWhitelistNumber ZERO = () -> 0;
+
     int getTimesAutoWhitelisted();
 
-    boolean isExceeded(final int limit);
+    default boolean isExceeded(final int limit) {
+        return (limit >= 0) && (getTimesAutoWhitelisted() >= limit);
+    }
 }
