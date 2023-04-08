654b12170c8b1238c63bfd1a515808e8422170fd
Vasiliy <void.pointer@ya.ru>
2023-04-08 19:40:15 +0500

    Add method `#add()` with AutoWhitelistNumber

--- src/main/java/voidpointer/spigot/voidwhitelist/storage/AutoWhitelistService.java
+++ src/main/java/voidpointer/spigot/voidwhitelist/storage/AutoWhitelistService.java
@@ -1,11 +1,16 @@
 package voidpointer.spigot.voidwhitelist.storage;
 
 import voidpointer.spigot.voidwhitelist.AutoWhitelistNumber;
+import voidpointer.spigot.voidwhitelist.Whitelistable;
 
+import java.util.Date;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 public interface AutoWhitelistService extends WhitelistService {
     CompletableFuture<Optional<AutoWhitelistNumber>> getAutoWhitelistNumberOf(final UUID uniqueId);
+
+    CompletableFuture<Optional<Whitelistable>> add(final UUID uuid, final String name, final Date expiresAt,
+                                                   final AutoWhitelistNumber autoWhitelistNumber);
 }
